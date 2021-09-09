c13d424bd88b32f47c6c1d7b07a774c4d0621ad0
scottkao177 <scottkao177@gmail.com>
2021-09-08 17:49:04 -0700

    commit

--- 01-java-fundamentals/05-basics/Basics.java
+++ 01-java-fundamentals/05-basics/Basics.java
@@ -1,18 +1,57 @@
 import java.util.ArrayList;
 public class Basics {
     public static void main(String[] args) {
-
         // Print 1 - 255
         for (int i = 0; i < 256; i++) {
             System.out.println(i);
         }
-
         // Print Odd numbers 1 - 255
         for (int i = 0; i < 256; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
-
+        // Print Sum of 255
+        Integer sum = 0;
+        for(int i = 0; i < 256; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        // iterate Array
+        int[] myArray = {1,3,5,7,9,13};
+        for(int i = 0; i <= myArray.length; i++) {
+            System.out.println(i);
+        }
+        // Max Value of Array
+        int[] arr = {-3,-5,-7, 10};
+        int arrayMax = arr[0];
+        for(int i = 0; i <= arr.length; i++) {
+            try {
+                if (arr[i] > arrayMax) {
+                    arrayMax = arr[i];
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println(arrayMax);
+        // Array of Odd Numbers 1 - 255
+        int[] oddArray = {0};
+        for (int i = 0; i < 256; i++) {
+            if (i % 2 != 0) {
+                oddArray += i;
+            }
+        }
+        System.out.println(oddArray);
     }
+
+    // Return Sum of 255
+    public static int sum() {
+        Integer sum = 0;
+        for(int i = 0; i < 256; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
 }
